948c206863e6642da2172083260e16ebd630eec8
pxax5 <jiraphat9298@gmail.com>
2025-12-19 14:15:08 +0700

    lab8_3 is complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,60 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+
+int main(){
+    int age;
+    float height;
+    double bounty;
+
+
+    cout << "Enter your age: " ;
+    cin >> age;
+
+
+    if(age <= 25 ){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100.0 ){
+            cout << "Your character = Chopper ";
+            }
+        else if(height < 180.0 ){
+            cout << "Your character = Usopp";
+            }
+        else if(height >= 180.0 ){
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 1100000000.0){
+              cout << "Your character = Zoro";
+              }
+            else{
+              cout << "Your character = Sanji";
+        
+            }
+
+        }
+    }    
+    else if(age > 25){
+        if(age <= 60){
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 500000000.0){
+              cout << "Your character = Jinbe";
+              }
+            else{
+              cout << "Your character = Franky";
+            }
+        }
+        else{
+            cout << "Your character = Brook";
+        }
+    }
+
+    return 0;
+}
+
 /*
 "Enter your age: "
 "Enter your height: "
